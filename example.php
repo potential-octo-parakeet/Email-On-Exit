81026b8a260d77c90fb63245129a9faed583d968
Mar Cejas <mvcejas@gmail.com>
2012-12-11 19:18:15 -0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Email On Exit</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>
+	var WEBDATA = {
+		email: 'mvcejas@gmail.com',/* user email. default must set to <?=$_SESSION['form_data1']['email']?> */
+		subject: 'Please complete your BuySurety form.',/* email subject */
+		message: '<h1>Thank you for visiting buysurety</h1>'/* email body, accept HTML */
+	};
+
+	window.onbeforeunload = confirmExit;
+	function confirmExit(){
+		$.post('pushmail.php',{
+			email: WEBDATA.email,
+			subject: WEBDATA.subject,
+			message: WEBDATA.message
+		});
+	}
+
+  WEBDATA.email = prompt("Enter your email","me@mvcejas.com");
+
+	</script>
+</head>
+<body>
+<h1>This example will email you once you leave this page.</h1>
+</body>
+</html>
